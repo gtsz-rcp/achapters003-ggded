aac895e74b71b4cca8dc14e339d6146d9f776e37
Seung-hae KIM <aka.seunghaekim@gmail.com>
2018-05-19 16:41:32 +0900

    Split manuscripts to each project

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import BartelbyMachine.main as bartelby
-# from bartelby import Bartelby
+import BartlebyMachine.main as bartleby
+# from bartleby import Bartelby
 
-bartleby1 = bartelby.Bartelby()
-bartleby1.addTableOfContent('toc.lump-of-meat.json');
-bartleby1.markdowntolatex()
-
-bartleby2 = bartelby.Bartelby()
-bartleby2.addTableOfContent('toc.neighbors.json');
-bartleby2.markdowntolatex()
-
-bartleby3 = bartelby.Bartelby()
-bartleby3.addTableOfContent('toc.weathers.json');
-bartleby3.markdowntolatex()
-
-bartleby4 = bartelby.Bartelby()
-bartleby4.addTableOfContent('toc.cell.json');
-bartleby4.markdowntolatex()
+bartleby = bartleby.Bartelby()
+bartleby.addTableOfContent('toc.ggded.json');
+bartleby.markdowntolatex()
