eb600b81f0b184e8e036f591bcc33934faefb960
Seung-hae KIM <aka.seunghaekim@gmail.com>
2018-05-22 01:43:38 +0900

    CLEAR CACHE

--- main.py
+++
@@ -1,8 +0,0 @@
-import BartlebyMachine.main as bartleby
-# from bartleby import Bartleby
-
-config = bartleby.Config('config.yaml')
-bartleby = bartleby.Bartleby(config)
-bartleby.addTableOfContent('toc.ggded.yaml')
-bartleby.markdownToLatex()
-bartleby.writeLatex()
